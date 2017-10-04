d3608cb61b73118a1da0a5053d0892ada569589a
Apipia <anthony.92@live.com>
2017-10-04 10:27:32 -0700

    Added make file and test.cpp

---
+++ src/test.cpp
@@ -0,0 +1,13 @@
+/*
+# This file is used for testing the various functions
+# and operations used throughout the project
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Testing" << std::endl;
+	return 0;
+}
