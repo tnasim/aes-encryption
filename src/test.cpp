0565c8f0207ab7ba34bf15dd1c79a04dba528ccb
Apipia <anthony.92@live.com>
2017-10-18 10:15:12 -0700

    Added state header and cpp

--- src/test.cpp
+++ src/test.cpp
@@ -5,9 +5,21 @@
 
 #include <iostream>
 #include <stdio.h>
+#include "util/util.h"
 
 int main(int argc, char** argv)
 {
-	std::cout << "Testing" << std::endl;
+	if (argc < 3) 
+	{
+		std::cout << "Need arguments: <key-size> <text to encrypt>"
+		exit(1);
+	}
+	int keysize = atoi.(argv[1]);
+
+
+	unsigned char[][] state;
+	char foo[4] = {'f','o','o',0};
+	std::string output = util::charToHex(foo, 4);
+	std::cout << "Testing: " << output << std::endl;
 	return 0;
 }
